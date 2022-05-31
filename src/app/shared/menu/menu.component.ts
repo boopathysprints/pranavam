570a1397847fb877e8cc21b38ed681899ec7676f
boopathysprints <boopathysprints@gmail.com>
2022-05-31 11:36:10 +0530

    Dynamic links for menu items

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/interfaces/menuitem.interface';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  menuItems : MenuItem[] = [{name:"Dashboard",icon:"sliders",link:""},
+                            {name:"Lord In House",icon:"user",link:"/pranavam/lordinhouse"}];
+  selectedItem : string = "Dashboard";
+  
+  
+  constructor() {
+    
+   }
+
+  
 
   ngOnInit(): void {
   }
+
+  listClick(event, newValue) {
+    console.log(newValue);
+    this.selectedItem = newValue.name;  
+    // don't forget to update the model here
+    // ... do other stuff here ...
+}
 
 }
